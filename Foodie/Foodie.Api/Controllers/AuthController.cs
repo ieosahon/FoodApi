7c8d83bff9d9719edd089ccb2e37d4fb2d110fa6
Osahon Ighodaro <osahonie@gmail.com>
2022-06-14 03:42:52 +0100

    modified controller

--- Foodie/Foodie.Api/Controllers/AuthController.cs
+++ Foodie/Foodie.Api/Controllers/AuthController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Foodie.Contracts.Auth.AuthRequest;
 using Foodie.Foodie.Contracts.Auth.AuthRequest;
 using Foodie.Application.Services.Auth.Interface;
+using Foodie.Application.Services.Auth;
 
 namespace Foodie.Api.Controllers
 
@@ -20,13 +21,32 @@ namespace Foodie.Api.Controllers
         [HttpPost("register")]
         public IActionResult Register(RegisterReq req)
         {
-            return Ok(req); 
+            var userResult = _authService.UserRegister(req.FirstName, req.LastName, req.Email, req.Password);
+            // map user result values to the api response
+            var response = new AuthResult
+            (
+                userResult.Id,
+                userResult.LastName,
+                userResult.FirstName,
+                userResult.Email,
+                userResult.Token
+            );
+            return Ok(response); 
         }
 
         [HttpPost("login")]
         public IActionResult Login(LoginReq req)
         {
-            return Ok(req);
+            var user = _authService.UserLogin(req.Email, req.Password);
+            var userResponse = new AuthResult
+            (
+                user.Id,
+                user.FirstName,
+                user.LastName,
+                user.Email,
+                user.Token
+            );
+            return Ok(userResponse);
         }
     }
 }
